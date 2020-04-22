50a326939e421e87df87ae6abe75a94ae6d42c61
Douglas Lanca <douglas.lanca@avenuecode.com>
2020-04-21 20:45:13 -0700

    Testing the first domain class

--- src/main/java/com/dlanca/cursomc/resources/CategoryResource.java
+++ src/main/java/com/dlanca/cursomc/resources/CategoryResource.java
@@ -1,15 +1,26 @@
 package com.dlanca.cursomc.resources;
 
+import com.dlanca.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="categories")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listing(){
-        return "restornando";
+    public List<Category> listing(){
+
+        Category cat1 = new Category(1, "informatica");
+        Category cat2 = new Category(2, "escritorio");
+
+        List<Category> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 }
