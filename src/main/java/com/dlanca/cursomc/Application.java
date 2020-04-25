4c89428fa6bc23ae339d631d47288f519046690a
Douglas Lanca <douglas.lanca@avenuecode.com>
2020-04-25 14:59:31 -0700

    Criando intanciacao do BD

--- src/main/java/com/dlanca/cursomc/Application.java
+++ src/main/java/com/dlanca/cursomc/Application.java
@@ -1,13 +1,29 @@
 package com.dlanca.cursomc;
 
+import com.dlanca.cursomc.domain.Category;
+import com.dlanca.cursomc.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informatica");
+        Category cat2 = new Category(null, "Escritorio");
+
+        categoryRepository.save(Arrays.asList(cat1, cat2));
+    }
 }
