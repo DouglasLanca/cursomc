91f76b67a3259fedec0cacb81b96ad8ca624d5fc
Douglas Lanca <douglas.lanca@avenuecode.com>
2020-06-21 19:54:36 -0700

    Chapter 6 - Forgot the password

--- src/main/java/com/dlanca/cursomc/config/SecurityConfig.java
+++ src/main/java/com/dlanca/cursomc/config/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {
-            "/customers/**"
+            "/customers/**",
+            "/auth/forgot/**"
     };
 
     @Override
--- src/main/java/com/dlanca/cursomc/resources/AuthResource.java
+++ src/main/java/com/dlanca/cursomc/resources/AuthResource.java
@@ -1,19 +1,26 @@
 package com.dlanca.cursomc.resources;
 
+import com.dlanca.cursomc.dto.EmailDTO;
 import com.dlanca.cursomc.security.JWTUtil;
 import com.dlanca.cursomc.security.UserSpringSecurity;
+import com.dlanca.cursomc.services.AuthService;
 import com.dlanca.cursomc.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthResource {
+
+    @Autowired
+    private AuthService authService;
 
     @Autowired
     private JWTUtil jwtUtil;
@@ -23,6 +30,12 @@ public class AuthResource {
         UserSpringSecurity user = UserService.authenticated();
         String token = jwtUtil.generateToken(user.getUsername());
         response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/forgot", method= RequestMethod.POST)
+    public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+        authService.sendNewPassword(objDTO.getEmail());
         return ResponseEntity.noContent().build();
     }
 }
