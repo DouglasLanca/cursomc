7ee374926a85b8b2fab5160719f4bc40f9275b66
DouglasLanca <douglasvandersar@gmail.com>
2020-08-20 17:42:14 -0300

    Chapter 7 - AWS configured

--- src/main/java/com/dlanca/cursomc/Application.java
+++ src/main/java/com/dlanca/cursomc/Application.java
@@ -1,5 +1,7 @@
 package com.dlanca.cursomc;
 
+import com.dlanca.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -14,5 +18,6 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        s3Service.uploadFile("/Users/douglaslanca/IdeaProjects/Study/cursofullstack/spbob.png");
     }
 }
